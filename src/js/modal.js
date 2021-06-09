66dcdfd8f15c453eee86ac29101ef568e8cd70bf
Oleh <olegkaraaa@gmail.com>
2021-06-09 18:29:05 +0300

    Добавление модального окна, общие стили для модельного окна

--- src/js/modal.js
+++ src/js/modal.js
@@ -15,4 +15,61 @@
       refs.modal.classList.toggle('is-hidden')
 
     }
+})();
+
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open2]'),
+      closeModalBtn: document.querySelector('[data-modal-close2]'),
+      modal: document.querySelector('[data-modal2]'),
+    };
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+
+    function toggleModal() {
+      document.body.classList.toggle("modal-open");
+      refs.modal.classList.toggle('is-hidden')
+
+    }
+  })();
+  
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-about-open]'),
+      closeModalBtn: document.querySelector('[data-modal-about-close]'),
+      modal: document.querySelector('[data-modal-about]'),
+    };
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+
+    function toggleModal() {
+      document.body.classList.toggle("modal-open");
+      refs.modal.classList.toggle('is-hidden')
+
+    }
+})();
+  
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-bio-open]'),
+      closeModalBtn: document.querySelector('[data-modal-bio-close]'),
+      modal: document.querySelector('[data-modal-bio]'),
+    };
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+
+    function toggleModal() {
+      document.body.classList.toggle("modal-open");
+      refs.modal.classList.toggle('is-hidden')
+
+    }
   })();
