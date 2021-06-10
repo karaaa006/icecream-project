a9928caa916ff81a2c9191c38fc5bd99301bd1cc
Oleh <olegkaraaa@gmail.com>
2021-06-10 20:51:45 +0300

    more

--- src/js/modal.js
+++ src/js/modal.js
@@ -72,4 +72,19 @@
       refs.modal.classList.toggle('is-hidden')
 
     }
+})();
+  
+(() => {
+    const refs = {
+      productItem: document.querySelectorAll('.js-products__item'),
+    };
+    refs.productItem.forEach(item => {
+      item.addEventListener('click', event => {
+        const tag = event.target.tagName;
+        if (tag === 'BUTTON') {
+          event.path[1].children[3].classList.toggle('is-hidden');
+          event.target.classList.toggle('is-click');
+        }
+      });
+    });
   })();
